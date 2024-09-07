d803898d6bfe2fd99090b1304bc27cff20f1e312
kachraz <kachraz@tuta.io>
2024-09-07 14:24:52 +0530

    tailwind classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.jpg";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
